3e45419ff8db1348a63da121bd5d233d8e9675e3
Hugo <gthugo@outlook.com>
2022-06-07 19:11:06 -0500

    Adding views for the app

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -2,6 +2,18 @@ from django.urls import path
 
 from . import views
 
+
+app_name = "polls"
 urlpatterns = [
-    path('', views.index, name='index')
+    #ex: /polls/
+    path('', views.index, name='index'),
+
+    #ex: /polls/6/
+    path('<int:question_id>/', views.detail, name='detail'),
+
+    #ex: /polls/6/results
+    path('<int:question_id>/results/', views.results, name='results'),
+
+    #ex: /polls/6/vote
+    path('<int:question_id>/vote', views.vote, name='vote'),
 ]
--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
+from . models import Question
+
 def index(request):
-    return HttpResponse("Hello World")
+    latest_question_list = Question.objects.all()
+    return render(
+        request,
+        "polls/index.html",
+        {"latest_question_list" : latest_question_list}
+    )
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(
+        request,
+        "polls/detail.html",
+        {"question" : question}
+        )
+
+
+def results(request, question_id):
+    return HttpResponse(f"Estás viendo los resultados de la pregunta número {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"Estás votando a la pregunta número {question_id}")
