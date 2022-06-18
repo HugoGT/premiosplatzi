3c8877965a375cebe57f56e4a1f02a986ed1c1bd
Hugo <gthugo@outlook.com>
2022-06-17 22:00:34 -0500

    Improving the Django-Admin

--- premiosplatziapp/polls/admin.py
+++ premiosplatziapp/polls/admin.py
@@ -1,5 +1,18 @@
 from .models import Question, Choice
 from django.contrib import admin
 
-admin.site.register(Question)
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]
+    inlines = [ChoiceInLine]
+    list_display = ("question_text", "pub_date", "was_published_recently")
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
